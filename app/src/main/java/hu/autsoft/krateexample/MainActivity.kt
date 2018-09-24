75d655c93162807ff5644ce139c6463fb6d122e5
Braun Márton Szabolcs <braun.marton@autsoft.hu>
2018-09-24 08:31:13 +0200

    Added initial version of example app

--- app/src/main/java/hu/autsoft/krateexample/MainActivity.kt
+++ app/src/main/java/hu/autsoft/krateexample/MainActivity.kt
@@ -1,13 +1,26 @@
 package hu.autsoft.krateexample
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSimpleKrate.setOnClickListener {
+            val intent = Intent(this, ExampleActivity::class.java)
+                    .putExtra(ExampleActivity.KEY_KRATE_TYPE, ExampleActivity.TYPE_SIMPLE)
+            startActivity(intent)
+        }
+        btnCustomKrate.setOnClickListener {
+            val intent = Intent(this, ExampleActivity::class.java)
+                    .putExtra(ExampleActivity.KEY_KRATE_TYPE, ExampleActivity.TYPE_CUSTOM)
+            startActivity(intent)
+        }
     }
 
 }
