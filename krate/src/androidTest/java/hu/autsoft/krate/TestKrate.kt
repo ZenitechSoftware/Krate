70c1120c188d2269717d0f83efcdaf24ca916f53
Braun Márton Szabolcs <braun.marton@autsoft.hu>
2018-09-26 15:19:28 +0200

    Added instrumentation test setup and test for optional preferences

---
+++ krate/src/androidTest/java/hu/autsoft/krate/TestKrate.kt
@@ -0,0 +1,14 @@
+package hu.autsoft.krate
+
+import android.content.Context
+
+class TestKrate(context: Context) : SimpleKrate(context) {
+
+    var optionalBoolean by booleanPref("optionalBoolean")
+    var optionalFloat by floatPref("optionalFloat")
+    var optionalInt by intPref("optionalInt")
+    var optionalLong by longPref("optionalLong")
+    var optionalString by stringPref("optionalString")
+    var optionalStringSet by stringSetPref("optionalStringSet")
+
+}
