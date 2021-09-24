1936c249ce3657afa89882b08fc1dd094f89461b
Ivády István <ivady.istvan@autsoft.hu>
2021-09-24 13:34:24 +0200

    Update Example app to follow API changes

--- app/src/main/java/hu/autsoft/krateexample/krates/ExampleCustomKrate.kt
+++ app/src/main/java/hu/autsoft/krateexample/krates/ExampleCustomKrate.kt
@@ -4,13 +4,12 @@ package hu.autsoft.krateexample.krates
 
 import android.content.Context
 import android.content.SharedPreferences
-import hu.autsoft.krate.Krate
-import hu.autsoft.krate.booleanPref
-import hu.autsoft.krate.floatPref
-import hu.autsoft.krate.intPref
-import hu.autsoft.krate.longPref
-import hu.autsoft.krate.stringPref
-import hu.autsoft.krate.stringSetPref
+import hu.autsoft.krate.*
+import hu.autsoft.krate.default.withDefault
+import hu.autsoft.krate.gson.gsonPref
+import hu.autsoft.krate.kotlinx.kotlinxPref
+import hu.autsoft.krate.moshi.moshiPref
+import hu.autsoft.krateexample.models.User
 
 class ExampleCustomKrate(context: Context) : Krate, ExampleSettings {
 
@@ -20,11 +19,14 @@ class ExampleCustomKrate(context: Context) : Krate, ExampleSettings {
         sharedPreferences = context.applicationContext.getSharedPreferences("custom_krate_prefs", Context.MODE_PRIVATE)
     }
 
-    override var exampleBoolean by booleanPref("exampleBoolean", false)
-    override var exampleFloat by floatPref("exampleFloat", 0f)
-    override var exampleInt by intPref("exampleInt", 0)
-    override var exampleLong by longPref("exampleLong", 0L)
-    override var exampleString by stringPref("exampleString", "")
-    override var exampleStringSet by stringSetPref("exampleStringSet", setOf())
+    override var exampleBoolean by booleanPref("exampleBoolean").withDefault(false)
+    override var exampleFloat by floatPref("exampleFloat").withDefault(0f)
+    override var exampleInt by intPref("exampleInt").withDefault(0)
+    override var exampleLong by longPref("exampleLong").withDefault(0L)
+    override var exampleString by stringPref("exampleString").withDefault("")
+    override var exampleStringSet by stringSetPref("exampleStringSet").withDefault(setOf())
+    override var exampleUserGson by gsonPref<User>("user_gson").withDefault(User("Gson", "Smith"))
+    override var exampleUserKotlinX by kotlinxPref<User>("user_kotlinx").withDefault(User("KotlinX", "Smith"))
+    override var exampleUserMoshi by moshiPref<User>("user_moshi").withDefault(User("Moshi", "Smith"))
 
 }
