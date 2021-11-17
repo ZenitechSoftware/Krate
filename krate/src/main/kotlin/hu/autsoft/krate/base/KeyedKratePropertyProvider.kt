1bd2e3d0a9b606f205b671f89c15b58433e7755d
Márton Braun <braunmarci@gmail.com>
2021-11-17 19:51:06 +0100

    Add KDocs

--- krate/src/main/kotlin/hu/autsoft/krate/base/KeyedKratePropertyProvider.kt
+++ krate/src/main/kotlin/hu/autsoft/krate/base/KeyedKratePropertyProvider.kt
@@ -3,4 +3,7 @@ package hu.autsoft.krate.base
 import hu.autsoft.krate.Krate
 import kotlin.properties.PropertyDelegateProvider
 
+/**
+ * A [PropertyDelegateProvider] that always provides [KeyedKrateProperty] instances.
+ */
 public interface KeyedKratePropertyProvider<T> : PropertyDelegateProvider<Krate, KeyedKrateProperty<T>>
