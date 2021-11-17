4ad667434ce72356db58a904ac5d5d4ce1a56711
Márton Braun <braunmarci@gmail.com>
2021-11-17 18:26:47 +0100

    Add Kotlin binary validator plugin

--- krate-moshi-stub/src/main/kotlin/hu/autsoft/krate/moshi/DefaultMoshiInstance.kt
+++ krate-moshi-stub/src/main/kotlin/hu/autsoft/krate/moshi/DefaultMoshiInstance.kt
@@ -1,8 +1,10 @@
 package hu.autsoft.krate.moshi
 
 import com.squareup.moshi.Moshi
+import hu.autsoft.krate.internal.InternalKrateApi
 
 @Suppress("IMPLICIT_NOTHING_TYPE_ARGUMENT_IN_RETURN_POSITION")
+@InternalKrateApi
 public val defaultMoshi: Moshi by lazy {
     TODO("""
         This is a compile time placeholder so that defaultMoshi can be resolved in the core module.
