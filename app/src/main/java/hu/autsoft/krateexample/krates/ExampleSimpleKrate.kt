f807806a374f88e4d1e9a0d58e63440739ddc7d9
Márton Braun <braunmarci@gmail.com>
2021-11-18 16:36:09 +0100

    Clean app sample app changes

--- app/src/main/java/hu/autsoft/krateexample/krates/ExampleSimpleKrate.kt
+++ app/src/main/java/hu/autsoft/krateexample/krates/ExampleSimpleKrate.kt
@@ -1,11 +1,17 @@
 package hu.autsoft.krateexample.krates
 
 import android.content.Context
-import hu.autsoft.krate.*
+import hu.autsoft.krate.SimpleKrate
+import hu.autsoft.krate.booleanPref
 import hu.autsoft.krate.default.withDefault
+import hu.autsoft.krate.floatPref
 import hu.autsoft.krate.gson.gsonPref
+import hu.autsoft.krate.intPref
 import hu.autsoft.krate.kotlinx.kotlinxPref
+import hu.autsoft.krate.longPref
 import hu.autsoft.krate.moshi.moshiPref
+import hu.autsoft.krate.stringPref
+import hu.autsoft.krate.stringSetPref
 import hu.autsoft.krateexample.models.User
 
 class ExampleSimpleKrate(context: Context) : SimpleKrate(context, NAME), ExampleSettings {
@@ -20,7 +26,7 @@ class ExampleSimpleKrate(context: Context) : SimpleKrate(context, NAME), Example
     override var exampleLong by longPref("exampleLong").withDefault(0L)
     override var exampleString by stringPref("exampleString").withDefault("")
     override var exampleStringSet by stringSetPref("exampleStringSet").withDefault(setOf())
-    override var exampleUserGson by gsonPref<User>("user_gson").withDefault(User("Gson", "Smith"))
-    override var exampleUserKotlinX by kotlinxPref<User>("user_kotlinx").withDefault(User("KotlinX", "Smith"))
-    override var exampleUserMoshi by moshiPref<User>("user_moshi").withDefault(User("Moshi", "Smith"))
+    override var exampleUserGson by gsonPref<User>("user_gson").withDefault(User("Gson", "Green"))
+    override var exampleUserKotlinX by kotlinxPref<User>("user_kotlinx").withDefault(User("KotlinX", "Klark"))
+    override var exampleUserMoshi by moshiPref<User>("user_moshi").withDefault(User("Moshi", "Miller"))
 }
